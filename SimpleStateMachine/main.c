56cec521d1c1df51307cf5839e69c1f5a5920d1b
becirp <44769169+becirp@users.noreply.github.com>
2020-03-16 16:22:09 +0100

    State machine prototype suggested. Not implemented.

--- SimpleStateMachine/main.c
+++ SimpleStateMachine/main.c
@@ -3,6 +3,25 @@
 
 //Simple state machine for coffee dispenser
 
+typedef enum
+{
+    STATE_IDLE,
+    STATE_COIN_INSERTED,
+    STATE_OPTION_SELECTED,
+    STATE_COIN_RETURNED,
+    MAX_STATES
+} State;
+
+typedef enum
+{
+    EVENT_INSERT_COIN,
+    EVENT_SELECT_OPTION,
+    EVENT_COFFEE_READY,
+    EVENT_CANCEL,
+    MAX_EVENTS
+} Event;
+
+
 //State handlers
 char idleStateHandler()
 {
@@ -30,30 +49,34 @@ struct Menu_State
     char (*pHandler)(void);
 };
 
+//Definition of array of menu state structures
 struct Menu_State menuStates[] = {
     {STATE_IDLE,          NULL, idleStateHandler()},
     {STATE_COIN_INSERTED, NULL, coinInsertedHandler()}
 };
 
-//typedef enum
-//{
-//    STATE_IDLE,
-//    STATE_COIN_INSERTED,
-//    STATE_OPTION_SELECTED,
-//    STATE_COIN_RETURNED,
-//    MAX_STATES
-//} State;
-//
-//typedef enum
-//{
-//    EVENT_INSERT_COIN,
-//    EVENT_SELECT_OPTION,
-//    EVENT_COFFEE_READY,
-//    EVENT_CANCEL,
-//    MAX_EVENTS
-//} Event;
+//Definition of array of structures for state transitions
+struct Menu_NextState nextStates[] = {
+    {STATE_IDLE,          EVENT_INSERT_COIN,    STATE_COIN_INSERTED},
+    {STATE_COIN_INSERTED, EVENT_SELECT_OPTION,  EVENT_CANCEL}
+};
 
-
+//Function for state transitions
+char StateMachine(char state,char stimuli)
+{
+    char nextstate = state;    // Default stay in same state
+    unsigned char i;
+    for (i=0; menu_nextstate[i].state; i++)
+    {
+        if (menu_nextstate[i].state == state && menu_nextstate[i].input == stimuli)
+        {
+            // This is the one!
+            nextstate = menu_nextstate[i].nextstate;
+            break;
+        }
+    }
+    return(nextstate);
+}
 
 int main()
 {

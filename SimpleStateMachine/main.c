5157d63f52bb8e8c26a78cec8f9781af6c4cb76e
becirp <44769169+becirp@users.noreply.github.com>
2020-03-17 13:37:27 +0100

    Simple state machine implementation.

--- SimpleStateMachine/main.c
+++ SimpleStateMachine/main.c
@@ -9,7 +9,7 @@ typedef enum
     STATE_COIN_INSERTED,
     STATE_OPTION_SELECTED,
     STATE_COIN_RETURNED,
-    MAX_STATES
+    STATE_ERROR
 } State;
 
 typedef enum
@@ -21,6 +21,8 @@ typedef enum
     MAX_EVENTS
 } Event;
 
+
+State currentState = STATE_IDLE;
 
 //State handlers
 char idleStateHandler()
@@ -51,35 +53,46 @@ struct Menu_State
 
 //Definition of array of menu state structures
 struct Menu_State menuStates[] = {
-    {STATE_IDLE,          NULL, idleStateHandler()},
-    {STATE_COIN_INSERTED, NULL, coinInsertedHandler()}
+    {STATE_IDLE,          NULL, idleStateHandler},
+    {STATE_COIN_INSERTED, NULL, coinInsertedHandler}
 };
 
 //Definition of array of structures for state transitions
 struct Menu_NextState nextStates[] = {
-    {STATE_IDLE,          EVENT_INSERT_COIN,    STATE_COIN_INSERTED},
-    {STATE_COIN_INSERTED, EVENT_SELECT_OPTION,  EVENT_CANCEL}
+    {STATE_IDLE,            EVENT_INSERT_COIN,    STATE_COIN_INSERTED},
+    {STATE_IDLE,            EVENT_CANCEL,         STATE_IDLE},
+    {STATE_COIN_INSERTED,   EVENT_SELECT_OPTION,  STATE_OPTION_SELECTED},
+    {STATE_COIN_INSERTED,   EVENT_CANCEL,         STATE_IDLE},
+    {STATE_OPTION_SELECTED, EVENT_COFFEE_READY,   STATE_IDLE},
 };
 
-//Function for state transitions
-char StateMachine(char state,char stimuli)
+//Function for state transitions.
+unsigned int StateMachine(unsigned int state, unsigned int stimuli)
 {
-    char nextstate = state;    // Default stay in same state
-    unsigned char i;
-    for (i=0; menu_nextstate[i].state; i++)
+    unsigned int nextState = state;    // Default stay in same state
+    unsigned int i;
+    unsigned int numberOfStates = 2;
+    for (i=0; i<numberOfStates; i++)
     {
-        if (menu_nextstate[i].state == state && menu_nextstate[i].input == stimuli)
+        if (nextStates[i].state == state && nextStates[i].event == stimuli)
         {
             // This is the one!
-            nextstate = menu_nextstate[i].nextstate;
+            nextState = nextStates[i].nextState;
             break;
         }
+        nextState = STATE_ERROR;
     }
-    return(nextstate);
+    return(nextState);
 }
 
 int main()
 {
-
+    unsigned int testStimuli = 0;
+    unsigned int newState = STATE_IDLE;
+    char stimuli;
+    //ubaciti scanf
+    newState = StateMachine(currentState, testStimuli);
+    currentState = newState;
+    printf("%d\n", newState);
     return 0;
 }
